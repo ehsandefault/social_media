b1be3e6315f854184898a1966aa9cb4bb3116026
monu <mdehsan873@gmail.com>
2022-04-27 19:08:13 +0530

    'upload'

--- social_media/accounts/urls.py
+++ social_media/accounts/urls.py
@@ -4,6 +4,6 @@ from .views import SignupView,Login,ResetPassword,UserDetailsView
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
     path('login/',Login.as_view(),name='login'),
-    path('reset/',ResetPassword.as_view(),name='reset_password'),
-    path('users/<str:username>',UserDetailsView.as_view(),name='users'),
+    path('reset_password/',ResetPassword.as_view(),name='reset_password'),
+    path('users_profile/<str:username>',UserDetailsView.as_view(),name='users_profile'),
 ]
