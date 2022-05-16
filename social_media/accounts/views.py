c18fc2f7491004980f5319452f64af97753dfc5d
monu <mdehsan873@gmail.com>
2022-05-16 09:35:57 +0530

    'upload'

--- social_media/accounts/views.py
+++ social_media/accounts/views.py
@@ -4,61 +4,50 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
-from .serializer import AccountRegistrationSerializer,AccountSerializer
+from .serializer import AccountRegistrationSerializer, AccountSerializer
 from .models import User
 
 
 # Create your views here.
 class UserDetailsView(APIView):
 
-    def get(self,request,username):
-        try:
+    def get(self, request, username):
+        if User.filter(username=username).exists():
             user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            newdict = {'not_found': True}
+            account_serializer = AccountSerializer(instance=user)
+            return Response(account_serializer.data)
+        else:
             return Response(data={'error': {'message': 'user not found'}}, status=status.HTTP_404_NOT_FOUND)
-
-        account_serializer = AccountSerializer(user)
-        return Response(account_serializer.data)
-
-
-
 
 
 class Login(APIView):
     def post(self, request):
         data = request.data
-        try:
-            username = data["email"]
-            password = data["password"]
-        except:
+        email = data.get('email', None)
+        password = data.get("password", None)
+        if email is None or password is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         # check if a user with such username or email exist
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-
-            try:
-                user = User.objects.get(email=username)
-                username = user.username
-            except  User.DoesNotExist:
-                return Response({'error': 'Such user was not found'},
-                                status=status.HTTP_404_NOT_FOUND)
-
-        # check to see if username with that password exist
+        user = None
+        if User.filter(username=email).exists():
+            user = User.objects.get(username=email)
+        if User.filter(email=email).exists():
+            user = User.objects.get(email=email)
+        if user is None:
+            return Response({'error': 'Such user was not found'},
+                            status=status.HTTP_404_NOT_FOUND)
+        username = user.username
         user = authenticate(username=username, password=password)
         if not user:
             return Response({'error': 'Incorrect password'}, status=status.HTTP_401_UNAUTHORIZED)
 
-        # return info of the user
         token, created = Token.objects.get_or_create(user=user)
         print(token.key)
         data = {
             "success": True,
             "token": token.key,
             "username": user.username,
-            "date_joined": user.created_on,
             "email": user.email,
 
         }
@@ -73,6 +62,7 @@ class SignupView(APIView):
         data = {}
         if serializer.is_valid():
             new_user = serializer.save()
+            Token.objects.create(user=new_user)
             data = {
                 'success': 'User created',
                 'User id': new_user.id,
@@ -80,6 +70,7 @@ class SignupView(APIView):
                 'User email': new_user.email,
                 'token': Token.objects.get(user=new_user).key,
             }
+
             return Response(data, status=status.HTTP_201_CREATED)
         data = serializer.errors
         return Response(data, status=status.HTTP_400_BAD_REQUEST)
@@ -87,18 +78,18 @@ class SignupView(APIView):
 
 class ResetPassword(APIView):
     def post(self, request):
-        try:
-            new_password = request.data['new_password']
-            old_password = request.data['password']
-            email = request.data['email']
-        except KeyError:
+        data = request.data
+        new_password = data.get('new_password', None)
+        old_password = data.get('password', None)
+        email = data.get('email', None)
+        if new_password is None or old_password is None or email is None:
             return Response(data={'message': 'This was an invalid request'}, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
+        current_user = None
+        if User.filter(email=email).exists():
             current_user = User.objects.get(email=email)
-        except User.DoesNotExist:
+        else:
             return Response(data={'message': 'This user does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        username=current_user.username
+        username = current_user.username
         user = authenticate(username=username, password=old_password)
         if not user:
             return Response({'error': 'Incorrect password'}, status=status.HTTP_401_UNAUTHORIZED)
