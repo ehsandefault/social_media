abee94990011ab5d7bbc16d095e60e7f7892857c
monu <mdehsan873@gmail.com>
2022-05-19 10:25:31 +0530

    'upload'

--- social_media/accounts/models.py
+++ social_media/accounts/models.py
@@ -16,8 +16,8 @@ class User(AbstractUser):
     profile_image = models.CharField(max_length=1000,
                                      default=PROFILE_AVTAR)
 
-    created_on = models.TimeField()
-    updated_on = models.TimeField()
+    created_on = models.TimeField(auto_now_add=True)
+    updated_on = models.TimeField(auto_now=True)
 
     def __str__(self):
         return self.username
